f0fca012d9007bbe799dd81a3e8762132ccc2b92
Bek Khaitbaev <bek.khaitbaev@onetwotrip.com>
2015-01-07 18:40:20 +0300

    added divshotfile

--- config/environment.js
+++ config/environment.js
@@ -4,9 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'recipes-front',
     environment: environment,
-    baseURL: '/search',
+    baseURL: '/',
     locationType: 'auto',
-    ENABLE_ALL_FEATURES: true,
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
