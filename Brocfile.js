3a805d70f6cd8cadc54396847638a4e8ca2f4424
Bek Khaitbaev <bek.khaitbaev@onetwotrip.com>
2015-01-11 15:16:56 +0300

    added autoprefixer and loading just want project need from animate.css

--- Brocfile.js
+++ Brocfile.js
@@ -2,10 +2,17 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp();
+var app = new EmberApp({
+  autoprefixer: {
+    browsers: ['last 3 versions', 'android 3'],
+    cascade: false
+  }
+});
 
 app.import('bower_components/normalize.css/normalize.css');
-app.import('bower_components/animate.css/animate.min.css');
+app.import('bower_components/animate.css/source/_base.css');
+app.import('bower_components/animate.css/source/attention_seekers/wobble.css');
+app.import('bower_components/animate.css/source/bouncing_exits/bounceOut.css');
 
 app.import('bower_components/nprogress/nprogress.js');
 app.import('bower_components/typeahead-hacked/type-ahead-hacked.js');
