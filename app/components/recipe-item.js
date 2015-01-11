30fb9e5855b580e2d9b7b5a54304e6ff08c1a618
Bek Khaitbaev <bek.khaitbaev@onetwotrip.com>
2015-01-12 00:45:57 +0300

    removed junk code

--- app/components/recipe-item.js
+++ app/components/recipe-item.js
@@ -3,11 +3,9 @@ import ClickOutsideMixin from '../mixins/click-outside';
 
 var host = 'http://recipes-bek.s3.amazonaws.com/blured/';
 
-export default Ember.Component.extend(ClickOutsideMixin,{
+export default Ember.Component.extend({
   classNames: ['recipe-item'],
   attributeBindings: ['style'],
-  isTagsHidden: true,
-  listenForClickOutside: true,
 
   style: function(){
     return 'background:' + this.get('background') + ' center center no-repeat;';
@@ -21,37 +19,5 @@ export default Ember.Component.extend(ClickOutsideMixin,{
       return '';
     }
   }.property('image'),
-
-  clickOutside: function(e){
-    this.set('isTagsHidden', true);
-  },
-
-  click: function(e){
-    var $container = $(e.target).parents('.tags-container')
-    if( $container.length ){
-      this.toggleProperty('isTagsHidden');
-    }
-  },
-
-  tagShowHide: function(){
-    var isTagsHidden = this.get('isTagsHidden');
-    this.$('.tags-container').stop(true).animate({
-      'max-width': isTagsHidden ? '35%' : '60%'
-    }, 800, function(){
-
-    }.bind(this));
-  }.observes('isTagsHidden'),
-
-  // adjustSizeOfItemContainer: function(){
-  //   var totalWidth = 0;
-  //   var $container = this.$('.tags-sub-container');
-  //   $container.find('.tag').each( function(){
-  //     var $this = $(this)
-  //     totalWidth += $this.outerWidth()
-  //       + parseFloat($this.css('margin-left'))
-  //       + parseFloat($this.css('margin-right'));
-  //   });
-  //   $container.width( (totalWidth / 3) + 40 );
-  // }.on('didInsertElement'),
 
 });
